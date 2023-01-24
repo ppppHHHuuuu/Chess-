93b0ffa4ef28e510b11b9d6712c043ce2a0d8e0e
Phu <87368494+ppppHHHuuuu@users.noreply.github.com>
2023-01-24 21:45:57 +0700

    Delete StateTransition.java

--- src/StateTransition.java
+++
@@ -1,9 +0,0 @@
-public interface StateTransition {
-    default void initGame() {}
-    void newGame();
-    void startGame();
-    void stopGame();
-    void pauseGame();
-    void resumeGame();
-    void destroyGame();
-}
